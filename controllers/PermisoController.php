79a2416afb2fc4c9e85aec6fa1994496504a766c
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-16 11:34:30 -0600

    Corrigiendo tipos de datos en respuesta del server

--- controllers/PermisoController.php
+++ controllers/PermisoController.php
@@ -2,34 +2,51 @@
 require_once __DIR__ . '/../models/Permiso.php';
 require_once __DIR__ . '/../views/response.php';
 
-class PermisoController {
+class PermisoController
+{
     private $permisoModel;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->permisoModel = new Permiso();
     }
 
-    public function getAllPermissions() {
+    public function getAllPermissions()
+    {
         // Devolver todos los permisos
         return $this->permisoModel->getAllPermissions();
     }
 
-    public function getPermissionByName($name) {
+    public function getPermissionByName($name)
+    {
         // Devolver el permiso con el nombre indicado
         return $this->permisoModel->getPermissionByName($name);
     }
 
-    public function addPermission($data) {
+    public function addPermission($data)
+    {
         // Agregar un permiso
-        return $this->permisoModel->addPermission($data);
+        if (!isset($data['name'])) {
+            return ["message" => "El nombre del permiso es requerido"];
+        }
+
+        $permissionName = $data['name'];
+        return $this->permisoModel->addPermission($permissionName);
     }
 
-    public function updatePermission($id, $data) {
-        // Actualizar un permiso
-        return $this->permisoModel->updatePermission($id, $data);
+    public function updatePermission($id, $data)
+    {
+        // Asegurarte de que el nombre del permiso esté establecido
+        if (!isset($data['name'])) {
+            return ["message" => "El nombre del permiso es requerido"];
+        }
+
+        $permissionName = $data['name'];
+        return $this->permisoModel->updatePermission($id, $permissionName);
     }
 
-    public function deletePermission($id) {
+    public function deletePermission($id)
+    {
         // Eliminar un permiso
         return $this->permisoModel->deletePermission($id);
     }
