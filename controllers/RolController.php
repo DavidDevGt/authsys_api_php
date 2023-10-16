a70ef6655adefc3b710fa0fbc944e062152d4c95
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-16 09:49:59 -0600

    controlador rol terminado

--- controllers/RolController.php
+++ controllers/RolController.php
@@ -11,29 +11,36 @@ class RolController {
 
     public function getAllRoles() {
         // Devolver todos los roles
+        return $this->rolModel->getAllRoles();
     }
 
     public function getRoleByName($name) {
         // Devolver el rol con el nombre
+        return $this->rolModel->getRoleByName($name);
     }
 
     public function addRole($data) {
         // Agregar un rol
+        return $this->rolModel->addRole($data['name'], $data['description']);
     }
 
     public function updateRole($id, $data) {
         // Actualizar un rol
+        return $this->rolModel->updateRole($id, $data['name'], $data['description']);
     }
 
     public function deleteRole($roleId) {
         // Eliminar un rol
+        return $this->rolModel->deleteRole($roleId);
     }
 
     public function assignPermission($roleId, $permissionId) {
         // Asignar un permiso a un rol
+        return $this->rolModel->assignPermission($roleId, $permissionId);
     }
 
     public function revokePermission($roleId, $permissionId) {
         // Revocar un permiso de un rol
+        return $this->rolModel->revokePermission($roleId, $permissionId);
     }
 }
