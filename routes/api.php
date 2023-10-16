087c9c8a1c9473bfa1c8d432aafefb8b6d805062
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-15 22:12:38 -0600

    refactorizar api con mas validaciones

--- routes/api.php
+++ routes/api.php
@@ -2,26 +2,45 @@
 require_once '../controllers/UsuarioController.php';
 require_once '../views/response.php';
 
-// Registro de usuario
 $usuarioController = new UsuarioController();
 
+// Verificar el método de la solicitud HTTP
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
+// Procesar la solicitud según el método HTTP y la acción proporcionada
 switch ($requestMethod) {
     case 'POST':
-        if ($_GET['action'] == 'register') {
-            $response = $usuarioController->register($_POST);
-            jsonResponse($response);
-        } elseif ($_GET['action'] == 'login') {
-            $response = $usuarioController->login($_POST);
-            jsonResponse($response);
-        } elseif ($_GET['action'] == 'verify') {
-            $response = $usuarioController->verify($_POST);
-            jsonResponse($response);
+        // Verificar si se proporciona una acción
+        if (isset($_GET['action'])) {
+            switch ($_GET['action']) {
+                case 'register':
+                    // Registrar un usuario
+                    $response = $usuarioController->register($_POST);
+                    jsonResponse($response);
+                    break;
+                case 'login':
+                    // Iniciar sesión de un usuario
+                    $response = $usuarioController->login($_POST);
+                    jsonResponse($response);
+                    break;
+                case 'verify':
+                    // Verificar la cuenta de un usuario
+                    $response = $usuarioController->verify($_POST);
+                    jsonResponse($response);
+                    break;
+                default:
+                    // Acción no reconocida
+                    jsonResponse(["message" => "Acción no permitida"], 400);
+                    break;
+            }
+        } else {
+            // No se proporcionó ninguna acción
+            jsonResponse(["message" => "Acción no especificada"], 400);
         }
         break;
 
     default:
+        // Método HTTP no permitido
         jsonResponse(["message" => "Método no permitido"], 405);
         break;
 }
