e844c02058e553678dab53df3d5c14cc0a853d1d
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-15 23:55:38 -0600

    Agregando CORS

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,13 @@
 <?php
+// Encabezados para soporte CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit;
+}
+
 require_once __DIR__ . '/../controllers/UsuarioController.php';
 require_once __DIR__ . '/../views/response.php';
 
