dc3c9b3f9db0eaebf45e6ab9ed7c18f5e09df83c
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-16 00:30:13 -0600

    constructores

--- controllers/PermisoController.php
+++ controllers/PermisoController.php
@@ -1,7 +1,11 @@
 <?php
-require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../models/Permiso.php';
 require_once __DIR__ . '/../views/response.php';
 
 class PermisoController {
-    
+    private $permisoModel;
+
+    public function __construct() {
+        $this->permisoModel = new Permiso();
+    }
 }
--- controllers/RolController.php
+++ controllers/RolController.php
@@ -1,7 +1,11 @@
 <?php
-require_once __DIR__ . '/../config/db.php';
+require_once __DIR__ . '/../models/Rol.php';
 require_once __DIR__ . '/../views/response.php';
 
 class RolController {
-    
+    private $rolModel;
+
+    public function __construct() {
+        $this->rolModel = new Rol();
+    }
 }
