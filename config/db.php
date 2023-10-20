a568be8c2f8d0927ebd6ae484fc87cafd1cb0ff9
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-20 11:06:28 -0600

    Declarar estandar caracteres por seguridad

--- config/db.php
+++ config/db.php
@@ -13,8 +13,12 @@ class Database
         $this->conn = null;
         try {
             $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            
+            // Establecer el conjunto de caracteres.
+            $this->conn->set_charset("utf8mb4");
+
         } catch (Exception $exception) {
-            echo "Connection error: " . $exception->getMessage();
+            echo "Error de conexión: " . $exception->getMessage();
         }
         return $this->conn;
     }
