27a2726c65b2daf1a5b17c597ad5a475d4dac402
DavidDevGt <josuedavidvl18@gmail.com>
2023-10-20 10:44:20 -0600

    Mejoras en las respuestas JSON

--- views/response.php
+++ views/response.php
@@ -1,8 +1,27 @@
 <?php
-// Formato JSON para respuestas del servidor
-function jsonResponse($data, $status = 200)
+/**
+ * Envía una respuesta en formato JSON al cliente.
+ *
+ * @param mixed $data Datos que serán enviados como respuesta.
+ * @param int $status Código de estado HTTP (por defecto es 200).
+ * @param array $headers Encabezados adicionales que se quieran agregar a la respuesta.
+ */
+function jsonResponse($data, $status = 200, $headers = [])
 {
+    // Establecer el tipo de contenido a JSON
     header('Content-Type: application/json');
+
+    // Establecer el código de respuesta HTTP
     http_response_code($status);
-    echo json_encode($data);
+
+    // Añadir encabezados adicionales si se proporcionan
+    foreach ($headers as $key => $value) {
+        header("$key: $value");
+    }
+
+    // Enviar la respuesta en formato JSON
+    echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 }
+
+// Lo deje como un array vacio para que sea opcional si queres mandar encabezados personalizados
+// o algo mas que no sea el status
